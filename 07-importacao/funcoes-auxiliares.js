45df21ae4235dd26eae580d1586eec57e4855f96
Alex Sandro <lexilva@gmail.com>
2023-10-12 18:19:55 -0300

    praticando os Conceitos de Importação e Exportação

--- 07-importacao/funcoes-auxiliares.js
+++ 07-importacao/funcoes-auxiliares.js
@@ -1,7 +1,11 @@
 
+const entradas =[5,50,10,98,23];
+let i = 0;
 
 function gets(){
-    return 10;
+    const valor = entradas[i];
+    i++;
+    return valor;
 }
 
 function print(texto){
--- 07-importacao/main.js
+++ 07-importacao/main.js
@@ -1,5 +1,33 @@
+/**
+ * uma sala contem 5 alunos e para cada aluno foi sorteado um número de 1 - 100.
+ * Faça um progama que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+ * 
+ * Dados de entrada:
+ * 5
+ * 50
+ * 10
+ * 98
+ * 23
+ * saída:
+ * 98
+ */
 
 
-const funcoes = require('./funcoes-auxiliares');
+const {gets,print} = require('./funcoes-auxiliares');
 
-console.log(funcoes.gets());
+const numerosSorteados =[];
+for (let i = 0; i < 5; i++) {
+   const numerosSorteado = gets();
+   numerosSorteados.push(numerosSorteado);
+}
+
+let maiorValor = -100000000;
+
+for (let i = 0; i < numerosSorteados.length; i++) {
+    const numerosSorteado = numerosSorteados[i];
+    if(numerosSorteado > maiorValor){
+        maiorValor = numerosSorteado;
+    }
+    
+}
+print(maiorValor);
