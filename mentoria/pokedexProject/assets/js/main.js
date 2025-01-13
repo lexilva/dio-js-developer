ce0040139b430de2218273bfd5a823b2778793cd
Alex Sandro <lexilva@gmail.com>
2025-01-13 20:26:37 -0300

    utilizando map para adicinar elementos html pelo javascript

--- mentoria/pokedexProject/assets/js/main.js
+++ mentoria/pokedexProject/assets/js/main.js
@@ -1,1 +1,28 @@
-console.log('Sucesso!!!')
+
+
+function convertPokemonToLi(pokemon) {
+    return `
+        <li class="pokemon">
+        <span class="number">#001</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                <li class="type">grass</li>
+                <li class="type">poison</li>
+                
+            </ol>
+
+            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg"
+                alt="${pokemon.name}">
+        </div>
+
+    </li>
+        `
+
+}
+
+const pokemonList = document.getElementById(`pokemonList`);
+
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
